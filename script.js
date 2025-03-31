235c5afc9972d6a293af7e4b3791d06bfa5f91d1
malin-leven <malin_leven@brown.edu>
2025-03-31 14:04:53 -0400

    comitting

---
+++ script.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: true,
+  grabCursor: true,
+  initialSlide: 1,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
